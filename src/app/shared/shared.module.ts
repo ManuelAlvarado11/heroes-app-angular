92784adbe17a6c333cb9749d32dcf2a911834d55
Manuel Alvarado <manuel.alvarado@bcr.gob.sv>
2024-02-20 11:35:51 -0600

    make components of material and shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [NotFoundPageComponent],
+
+  exports: [NotFoundPageComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
