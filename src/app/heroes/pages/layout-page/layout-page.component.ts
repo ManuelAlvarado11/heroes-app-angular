3cb6bac179c9866eef561f2b0c899d1a1b461acf
Manuel Alvarado <manuel.alvarado@bcr.gob.sv>
2024-02-21 14:26:36 -0600

    add sidenav/toolbar and opcion menu with routing

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
-  styleUrl: './layout-page.component.css'
+  styleUrl: './layout-page.component.css',
 })
 export class LayoutPageComponent {
-
+  public sidenavItems = [
+    {
+      label: 'Listado',
+      icon: 'list',
+      url: './list',
+    },
+    {
+      label: 'Anadir',
+      icon: 'add',
+      url: './new-hero',
+    },
+    {
+      label: 'Buscar',
+      icon: 'search',
+      url: './search',
+    },
+  ];
 }
