dd7f7ea5d7e96bfa6e2fdf48d4a0d0b32b3d8541
Manuel Alvarado <manuel.alvarado@bcr.gob.sv>
2024-02-22 10:41:06 -0600

    add HeroCardComponent

--- src/app/heroes/pages/list-page/list-page.component.ts
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-list-page',
   templateUrl: './list-page.component.html',
-  styleUrl: './list-page.component.css'
+  styleUrl: './list-page.component.css',
 })
-export class ListPageComponent {
+export class ListPageComponent implements OnInit {
+  public heroes: Hero[] = [];
 
+  constructor(private srvHeroes: HeroesService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes() {
+    this.srvHeroes.getHeroes().subscribe((data) => {
+      this.heroes = data;
+    });
+  }
 }
