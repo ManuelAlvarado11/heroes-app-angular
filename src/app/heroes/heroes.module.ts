f0a07c2f3649698a98ca93779199762ead456e3d
Manuel Alvarado <manuel.alvarado@bcr.gob.sv>
2024-03-26 10:36:33 -0600

    heroImage Pipe

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -9,16 +9,21 @@ import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { MaterialModule } from '../material/material.module';
 import { HeroCardComponent } from './components/hero-card/hero-card.component';
+import { HeroImagePipe } from './pipes/hero-image.pipe';
 
 
 @NgModule({
   declarations: [
+    // COMPONENTS
     HeroPageComponent,
     LayoutPageComponent,
     ListPageComponent,
     NewPageComponent,
     SearchPageComponent,
     HeroCardComponent,
+
+    // PIPES
+    HeroImagePipe,
 
   ],
   imports: [
